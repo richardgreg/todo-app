b73eb09bcd97da4fc9e9afeee068d1be1f024e91
Richard Gregory <richardgrecoson@gmail.com>
2023-03-03 10:20:36 +0100

    Add CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import {AiOutlinePlus} from 'react-icons/ai'
 import Todo from './Todo';
 
 const style = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#11f2dc] to-[#a3f0e8]`
-}
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-green-500 text-slate-100`,
+  count: `text-center p-2`,
+};
 
 function App() {
   // States
   const [todos, setTodos] = useState(['Learn React', 'Grind Leetcode'])
-
   return (
     <div className={style.bg}>
       <div className={style.container}>
