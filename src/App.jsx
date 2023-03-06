b4a061304460c4fc47a21a174896c349350afe0f
Richard Gregory <richardgrecoson@gmail.com>
2023-03-06 12:46:56 +0100

    Add delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 import {AiOutlinePlus} from 'react-icons/ai'
 import Todo from './Todo';
 import {db} from './firebase';
-import {query, collection, onSnapshot, updateDoc, doc, addDoc} from "firebase/firestore";
+import {query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc} from "firebase/firestore";
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
@@ -59,7 +59,9 @@ function App() {
 }
 
   // delete todo
-
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, 'todos', id))
+  }
 
   return (
     <div className={style.bg}>
@@ -76,7 +78,12 @@ function App() {
         </form>
         <ul>
           {todos.map((todo, index) => (
-            <Todo key={index} todo={todo} toggleComplete={toggleComplete}/>
+            <Todo
+              key={index}
+              todo={todo}
+              toggleComplete={toggleComplete}
+              deleteTodo={deleteTodo}
+            />
           ))}
         </ul>
         {todos.length < 1 ? null : <p className={style.count}>{`You have ${todos.length} todos`}</p>}
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -10,14 +10,14 @@ const style = {
   button: `cursor-pointer flex items-center`,
 };
 
-export default function Todo({todo, toggleComplete}) {
+export default function Todo({todo, toggleComplete, deleteTodo}) {
   return (
     <li className={todo.completed ? style.liComplete : style.li}>
       <div className={style.row}>
         <input onChange={() => toggleComplete(todo)} type="checkbox" checked={todo.completed ? 'checked' : ''}/>
         <p onClick={() => toggleComplete(todo)} className={todo.completed ? style.textComplete : style.text}>{todo.text}</p>
       </div>
-      <button>{<FaRegTrashAlt/>}</button>
+      <button onClick={() => (deleteTodo(todo.id))}>{<FaRegTrashAlt/>}</button>
     </li>
   )
 }
